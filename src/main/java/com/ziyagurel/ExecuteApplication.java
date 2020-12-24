5b3ab7479146e6715d23df26a7b7045f8669c596
ziyagurel <ziyagurel55@hotmail.com>
2020-12-25 00:21:15 +0300

    Created ExecuteApplication for spring project to run

---
+++ src/main/java/com/ziyagurel/ExecuteApplication.java
@@ -0,0 +1,11 @@
+package com.ziyagurel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecuteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExecuteApplication.class,args);
+    }
+}
