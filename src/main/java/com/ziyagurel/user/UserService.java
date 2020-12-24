510f9c993d5b81d64efb19bd1baf2fdbd9457047
ziyagurel <ziyagurel55@hotmail.com>
2020-12-25 00:05:37 +0300

    Created UserService. Added new method for user

---
+++ src/main/java/com/ziyagurel/user/UserService.java
@@ -0,0 +1,12 @@
+package com.ziyagurel.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+    public User getUserById(long id) {
+        return null;
+    }
+}
